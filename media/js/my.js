5f3199e22e6253dbb7320529b8272e9a8dd38e36
tanjusja <tanyxxxl91@mail.ru>
2016-04-06 19:15:45 +0300

    third

--- media/js/my.js
+++ media/js/my.js
@@ -8,12 +8,11 @@ $(function(){
 		});//прослушиватель bind (вход.параметр, принимает в колл-бэк функцию)
 		*/
 		$('.topmenu a').bind({'mouseover':function(){
-		data=$(this).attr('data-url'); 
-				
-		$('#header').css('background', 'url('+data+')');
+			data=$(this).attr('data-url'); 		
+			$('#header').css('background', 'url('+data+')');
 		},
 		'mouseout':function(){
-		$('#header').css('background','url(media/img/numbers5.jpg)');
+			$('#header').css('background','url(media/img/numbers5.jpg)');
 		},
 		});
 		
@@ -23,8 +22,18 @@ $(function(){
 			}	
 		});
 		$('.leftmenu a').bind({'click':function(){
-			data2=$(this).attr('data-body'); 		
-			$('.main').html(data2);
+			data2=$('<img>').attr('src','data-url');
+			$('.img_main').html(data2);
+			}
+		});
+		$('.leftmenu a').bind({'click':function(){
+			data3=$(this).attr('data-body'); 		
+			$('.main').html(data3);
 			}	
+		});
+		$('.call').bind({'mouseover':function(){
+			$(this).hide('data-title');
+			$('.button_call').show('fast');
+		}
 		});
 });
